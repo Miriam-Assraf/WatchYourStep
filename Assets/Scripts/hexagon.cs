422ca4fd06d9abdd997d3a3aa1c71e437aa4db2f
Miriam Assraf <miriam.assraf@gmail.com>
2021-01-15 19:58:40 +0200

    Merged with game scene

---
+++ Assets/Scripts/hexagon.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hexagon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int numOfHits;
+    public bool isHexagonTileBroken;
+    void Start()
+    {
+        numOfHits = 0;
+        isHexagonTileBroken = false;
+    }
+
+    void hit()
+    {
+        --numOfHits;
+        if (numOfHits == 3)
+            isHexagonTileBroken = true;
+    }
+}
